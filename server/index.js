01045937e52de416a7a8377ade91398f6d58da2c
Dmitry Vasilyev <vaseker@yandex-team.ru>
2015-12-18 03:57:46 +0500

    Fix of sock cleanup in each worker

--- server/index.js
+++ server/index.js
@@ -12,6 +12,17 @@ cluster.setupMaster({
 	args: process.argv
 });
 
+(function cleanupSocket(socket) {
+	if (socket && !(/^\d+$/.exec(socket))) {
+		require('mkdirp').sync(path.dirname(socket));
+
+		if (fs.existsSync(socket)) {
+			fs.unlinkSync(socket);
+		}
+	}
+	return socket;
+})(config.handle);
+
 for (var i = 0; i < config.workers; i++) {
 	cluster.fork();
 }
--- server/worker.js
+++ server/worker.js
@@ -13,20 +13,9 @@ app.enable('trust proxy');
 
 app.use(require('./router'));
 
-var handle = (function cleanupSocket(socket) {
-	if (socket && !(/^\d+$/.exec(socket))) {
-		require('mkdirp').sync(path.dirname(socket));
-
-		if (fs.existsSync(socket)) {
-			fs.unlinkSync(socket);
-		}
-	}
-	return socket;
-})(config.handle);
-
-app.listen(handle, function (err) {
+app.listen(config.handle, function (err) {
 	if (err) {
 		throw err;
 	}
-	console.log('app listening handle', handle);
+	console.log('app listening handle', config.handle);
 });
